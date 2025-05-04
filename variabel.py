6d1dc23bbdce80524fdee534ca52166b7c49837d
Carissa Feryna Hotmaregar <carissaferyna04@gmail.com>
2025-05-04 13:01:51 +0700

    materi variabel

---
+++ variabel.py
@@ -0,0 +1,18 @@
+print("BIODATA SAYA")
+print("\n") 
+namasaya = "Carissa Feryna Hotmaregar"
+print(namasaya)
+print("variabelnya adalah:", type(namasaya))
+print("\n") 
+umursaya = 16               
+print(umursaya)   
+print("variabelnya adalah:", type(umursaya)) 
+print("\n")            
+umur = "enam belas tahun" 
+print(umur)       
+print("variabelnya adalah:", type(umur))    
+print("\n")  
+kelas = 10
+print(kelas)   
+print("variabelnya adalah:", type(kelas))   
+
