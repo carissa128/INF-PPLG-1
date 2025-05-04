1bc79f8ed7e181f10332e917bce6dd4d7228d7d5
Carissa Feryna Hotmaregar <carissaferyna04@gmail.com>
2025-05-04 13:01:21 +0700

    materi tipe data

---
+++ tipeData.py
@@ -0,0 +1,10 @@
+pesanan = {"jenis": "sayuran", "jumlah": "20kg"}
+print(type(pesanan), pesanan)
+pembelianku = ["pulpen", "pensil", "buku", "penghapus"]
+print(type(pembelianku), pembelianku)
+pakaian = ("baju 2 lusin", "celana 1 stel", "sepatu 2 pasang")
+print(type(pakaian), pakaian)
+siapa = ("Halo, itu adalah codingan tentang tipe datakuu")
+print(type(siapa), siapa)
+tinggiku = (153,5)
+print(type(tinggiku), tinggiku)
