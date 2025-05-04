a37395a234cbb6a5ba776b25beb0cba5aa32c091
Carissa Feryna Hotmaregar <carissaferyna04@gmail.com>
2025-05-04 12:59:34 +0700

    materi komentar

---
+++ komentar.py
@@ -0,0 +1,15 @@
+#aku bakal munculin angka 1 sampai 10
+print ("puisi berjudul AKU dari Chairil Anwar")
+#ini adalah puisi yang aku dapatkan dari internet
+print ("Aku – Chairil Anwar")
+print ("(tahun 1943)")
+print ("Dari kumpulannya terbuangAku ini binatang jalang")
+print ("Dari kumpulannya terbuang")
+print ("Aku tetap meradang, menerjangBiar peluru menembus kulitku")
+print ("Aku tetap meradang, menerjang")
+print ("Berlari Hingga hilang pedih perihLuka dan bisa kubawa berlari")
+print ("Berlari")
+print ("Hingga hilang pedih perih")
+print ("Dan aku akan lebih tidak peduli")
+print ("Aku mau hidup seribu tahun lagi")
+#itulah puisi yang ku temui di internet dari Chairil Anwar
