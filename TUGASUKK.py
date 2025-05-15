25777371b84673ce1aba703dec69b5713389627f
Carissa Feryna Hotmaregar <carissaferyna04@gmail.com>
2025-05-15 04:02:09 -0500

    study kasus UKK

---
+++ TUGASUKK.py
@@ -0,0 +1,16 @@
+nilai_A = 70
+nilai_B = 80
+operator = "*"
+
+if(operator == "+"): 
+      hasil = nilai_A + nilai_B
+      print(hasil)
+elif(operator == "-"): 
+      hasil = nilai_A - nilai_B
+      print(hasil)
+elif(operator == "*"): 
+      hasil = nilai_A * nilai_B
+      print(hasil)
+elif(operator == "/"): 
+      hasil = nilai_A / nilai_B
+      print(hasil)      
